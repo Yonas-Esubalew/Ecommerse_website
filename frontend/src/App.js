f943849560a04b1d68ef24e77382e0e3f0ceada8
yonas pro <yonasbe999@gmail.com>
2025-04-12 23:55:54 -0900

    last commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,29 @@ import { Toaster } from "react-hot-toast";
 import { useEffect } from "react";
 import { setDataProduct } from "./redux/productSlice";
 import { useDispatch, useSelector } from "react-redux";
+
 function App() {
   const dispatch = useDispatch();
   const productData = useSelector((state) => state.product);
   useEffect(() => {
     (async () => {
-      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/product`);
-      const resData = await res.json();
-      
-      console.log(resData);
-      dispatch(setDataProduct(resData));
+      try {
+        const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/product`);
+        const contentType = res.headers.get("content-type");
+  
+        if (!contentType || !contentType.includes("application/json")) {
+          throw new Error("Response is not JSON");
+        }
+  
+        const resData = await res.json();
+        console.log(resData);
+        dispatch(setDataProduct(resData));
+      } catch (error) {
+        console.error("Failed to fetch product data:", error.message);
+      }
     })();
-
   }, [dispatch]);
-
+  
   console.log(productData);
   return (
     <>
